8fd9f62770f33467b65b01a9385813acc169a4cb
kayleepierce <143130896+kayleepierce@users.noreply.github.com>
2024-04-04 18:16:36 -0400

    Create compoundCalc.h

---
+++ Summer 2023/CPSC 1020/Retirement Calculator/compoundCalc.h
@@ -0,0 +1,12 @@
+// My Name: Kaylee Pierce
+// My Class: CPSC 1021
+// Date: 05/30/2023
+// Desc: This is the header file for retirementCalc.cpp
+// Time: 5 minutes
+
+#ifndef COMPOUNDCALC_H
+#define COMPOUNDCALC_H
+
+double compoundCalc(double principle, double interest, int years);
+
+#endif
